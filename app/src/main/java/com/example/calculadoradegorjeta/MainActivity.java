63c4322b0eb604adfe364c102ef979ea9bc4c47d
Rafael Varela <rafaelvrcr@gmail.com>
2021-06-02 18:03:45 -0300

    App concluido e rodando perfeitamente

--- app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
+                //Abaixo igualo o valor de porcentagem ao progress da seek bar
+                porcentagem = progress;
+                //abaixo eu uso o metodo Math.round para deixar o double porcentagem sem exibir decimal
+                textPorcentagem.setText(Math.round(porcentagem) + "%");
+                calcular();
+
             }
 
             @Override
@@ -47,4 +54,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public void calcular (){
+
+        String valorRecuperado = editValor.getText().toString();
+        if (valorRecuperado == null || valorRecuperado.equals("")){
+            Toast.makeText(
+                    getApplicationContext(),
+                    "digite um valor primeiro!",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            //converter string recuperado para double
+            double valorDigitado = Double.parseDouble( valorRecuperado );
+
+            //calcular a gorjeta total
+            double gorjeta = valorDigitado * (porcentagem/100);
+            double total = gorjeta + valorDigitado;
+            //exibir gorjeta e total
+            textGorjeta.setText("R$" + Math.round(gorjeta));
+            textTotal.setText("R$" + total);
+        }
+
+
+    }
+
 }
